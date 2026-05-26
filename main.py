e7ead8ef30a807b256722dd8f4734f844b9e0d97
drseverance <drseverance@outlook.com>
2026-05-25 23:24:21 -0500

    Add player

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    player = Player(
+        SCREEN_WIDTH / 2,
+        SCREEN_HEIGHT / 2,
+    )
 
     while True:
         log_state()
@@ -17,7 +26,11 @@ def main():
 
         screen.fill("black")
 
+        player.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 main()
